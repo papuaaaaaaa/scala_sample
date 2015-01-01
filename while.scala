db88fb3ce090db3cb04228bcd21d891a8e07d716
papua <nakao_shinichirou@aratana.jp>
2015-01-01 20:43:22 +0900

    while in section 2

---
+++ while.scala
@@ -0,0 +1,11 @@
+//exists better iterator
+
+var i = 0
+while (i < args.length) { //x while i<args.length
+  if (i!=0) {
+    print(" ")
+  }
+  print(args(i))
+  i += 1 //x i++
+}
+println()//o println();

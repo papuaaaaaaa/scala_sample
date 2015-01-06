1a494fc6b63091fa223356f479043eb66b9aff10
papua <nakao_shinichirou@aratana.jp>
2015-01-06 11:06:18 +0900

    constructor in section 1

---
+++ constructor.scala
@@ -0,0 +1,12 @@
+
+//等価なJavaコード
+//class MyClass {
+//  private int index;
+//  private String name;
+//  public MyClass(int index, String name) {
+//    this.index = index;
+//    this.name = name
+//  }
+//}
+
+class MyClass(index:Int, name:String)

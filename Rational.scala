fd99b716a6c1ca1e541e74365c03c200da129a70
papua <nakao_shinichirou@aratana.jp>
2015-01-06 14:05:52 +0900

    modify : add fields

--- Rational.scala
+++ Rational.scala
@@ -1,7 +1,12 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
+  private val _num = n
+  private val _denom = d
   println("created " + n + "/" + d )
+
   override def toString = n + "/" + d
+  def num:Int = _num
+  def denom:Int = _denom
 }
 
 

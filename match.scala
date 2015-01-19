4a6b47527160558e390c3903987ef8c568aa5422
papua <nakao_shinichirou@aratana.jp>
2015-01-19 11:17:56 +0900

    comment

--- match.scala
+++ match.scala
@@ -1,6 +1,7 @@
 object MatchTest {
   def matchTest(x :Any) = {
     x match {
+      //compile error
       case (_, _, _) => println("1=" + _ + ",2=" + _ + ",3=" + _ + ".")
       case _ =>
     }
